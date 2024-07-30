8eccbd4d7ed145ee0f19c9a140408ecbb0c4cdc3
dluman <dluman@allegheny.edu>
2024-07-30 08:56:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Graffito
+
+> Speak softly, but carry a big can of paint.
+> - Attributed to Banksy
+
+## Installtion
+
+Find this tool on`PyPI`: `pip install graffito`
+
+## Usage
+
+`TODO`
---
+++ src/graffito/tags.py
@@ -0,0 +1,45 @@
+import ast
+import sys
+import inspect
+
+from functools import wraps
+from types import FunctionType, ModuleType
+
+# This library is a (mis)use of some stuff I found on SO for another project.
+# I thought it might be useful, so I made it.
+
+# Decoration functionality taken from the SO:
+# https://stackoverflow.com/questions/3232024/introspection-to-get-decorator-names-on-a-method
+
+# Use the following to retrieve the original names
+# of wrapped functions:
+# https://stackoverflow.com/questions/4887081/get-the-name-of-a-decorated-function
+
+def tag(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return f(*args, **kwargs)
+    try:
+        tags = set_tags(f)
+        setattr(wrapper, "tags", tags)
+    except IndentationError as e:
+        pass
+    return wrapper
+
+def set_tags(obj):
+    # Is it polymorphic? Kinda.
+    target = obj
+    tags = {}
+    def visit_callable(node):
+        tags[node.name] = []
+        for n in node.decorator_list:
+            name = None
+            if isinstance(n, ast.Call):
+                name = n.func.attr if isinstance(n.func, ast.Attribute) else n.func.id
+            else:
+                name = n.attr if isinstance(n, ast.Attribute) else n.id
+            tags[node.name].append(name)
+    visitor = ast.NodeVisitor()
+    visitor.visit_FunctionDef = visit_callable
+    visitor.visit(ast.parse(inspect.getsource(target)))
+    return tags
